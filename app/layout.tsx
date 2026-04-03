7d593445722e900498fc5a80a9a1aaaa284b6ca1
Vidit Bhatnagar <viditbhatnagar53@gmail.com>
2026-04-04 04:18:42 +0530

    Uploading full project VAYU

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import "./globals.css";
+import VayuNetra from "@/components/VayuNetra";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
         <Providers>
           {children}
           <Toaster position="top-right" expand={true} richColors />
+          <VayuNetra />
         </Providers>
       </body>
     </html>
